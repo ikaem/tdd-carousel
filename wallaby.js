a52d73227f7dac51e18d06d34b4a7e806c9be825
Karlo Marinović <marinovic.karlo@gmail.com>
2020-11-01 11:57:24 +0100

    added os license

---
+++ wallaby.js
@@ -0,0 +1,16 @@
+// wallaby.js
+
+module.exports = (wallaby) => {
+  return {
+    testFramework: "jest",
+    env: {
+      type: "node",
+    },
+    tests: ["src/tests/**/*.test.js"],
+    files: ["src/**/*.js", "!**/*.test.js", "!**/.*"],
+
+    compilers: {
+      "**/*.js": wallaby.compilers.babel(),
+    },
+  };
+};
