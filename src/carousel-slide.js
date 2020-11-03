af69a1fca7a058ce60b1f794104b8a30347fd683
Karlo Marinović <marinovic.karlo@gmail.com>
2020-11-03 10:51:15 +0100

    initial webpack setup

--- src/carousel-slide.js
+++ src/carousel-slide.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CarouselSlide = ({ imgUrl, description, attribution, ...rest }) => {
+  console.log(attribution);
   return (
     <figure {...rest}>
       <img src={imgUrl} />
@@ -19,5 +20,5 @@ export default CarouselSlide;
 CarouselSlide.propTypes = {
   imgUrl: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  attribution: PropTypes.string.isRequired,
+  attribution: PropTypes.object.isRequired,
 };
